0e0e355e9cd2e2f9de0251f8555693012894bdbc
Carlos Eduardo <127683028+krarlos@users.noreply.github.com>
2023-11-15 00:27:46 +0000

    cambios

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -1,10 +1,23 @@
+// usare useStates , useEffect, setInterval, props en este proyecto
+import {  useState, useEffect } from "react";
 import SimpleCounter from "./SimpleCounter";
 
 
 function App() {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+      // componet mounting puts it in the browser
+      const interval = setInterval (() => {
+        setCounter(counter => counter + 1)
+      },1000)
+      console.log(counter)
+    }, [])
+    
   return (
     <>
     <SimpleCounter></SimpleCounter>
+    
     </>
   );
 }
